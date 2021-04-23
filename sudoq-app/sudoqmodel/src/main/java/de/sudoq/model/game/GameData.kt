e30f2125554ed1249712aaf86d582c49c0ce047d
timo <timo-a@gmx.ch>
2021-04-23 19:11:39 +0200

    converted GameData

---
+++ sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/game/GameData.kt
@@ -0,0 +1,60 @@
+/*
+ * SudoQ is a Sudoku-App for Adroid Devices with Version 2.2 at least.
+ * Copyright (C) 2012  Heiko Klare, Julian Geppert, Jan-Bernhard Kordaß, Jonathan Kieling, Tim Zeitz, Timo Abele
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.sudoq.model.game
+
+import de.sudoq.model.sudoku.complexity.Complexity
+import de.sudoq.model.sudoku.sudokuTypes.SudokuTypes
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Eine Klasse um die zu einem Spiel zugehörigen Daten zu sammeln ohne jeweils
+ * das ganze Spiel laden zu müssen.
+ *
+ * @param playedAt time when the game was last played
+ * @property id ID of the game
+ * @property type The type of the sudoku
+ * @property isFinished  Indicates if the game was completed
+ * @property complexity The complexit of the sudoku
+ */
+class GameData(val id: Int, playedAt: String, val isFinished: Boolean, val type: SudokuTypes, val complexity: Complexity) : Comparable<GameData> {
+
+    companion object {
+        const val dateFormat = "yyyy:MM:dd HH:mm:ss"
+    }
+
+    /**
+     * Date when the game was last played
+     */
+    val playedAt: Date
+
+    /**
+     * @throws IllegalArgumentException if playedAt cannot be parsed
+     */
+    init {
+        try {
+            this.playedAt = SimpleDateFormat(dateFormat).parse(playedAt)
+        } catch (e: ParseException) {
+            throw IllegalArgumentException(e)
+        }
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun compareTo(another: GameData): Int {
+        return if (isFinished == another.isFinished) {
+            playedAt!!.compareTo(another.playedAt)
+        } else {
+            if (isFinished) -1 else 1
+        }
+    }
+
+}
