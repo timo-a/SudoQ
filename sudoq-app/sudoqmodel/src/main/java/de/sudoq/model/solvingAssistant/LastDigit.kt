8d4911677050c3effd1f45c6f6bf6f3f336c6867
timo <timo-a@gmx.ch>
2021-04-28 13:17:56 +0200

    convert classes in solvingAssistant

---
+++ sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/solvingAssistant/LastDigit.kt
@@ -0,0 +1,37 @@
+package de.sudoq.model.solvingAssistant
+
+import de.sudoq.model.sudoku.Position
+import de.sudoq.model.sudoku.Sudoku
+import java.util.*
+
+/**
+ * Created by timo on 27.04.16.
+ */
+//TODO make an optional<SolveAction>
+object LastDigit {
+    fun findOne(sudoku: Sudoku): SolveAction? {
+        /* for every constraint */
+        for (c in sudoku.sudokuType!!) if (c.hasUniqueBehavior()) {
+            val v = Vector<Position>()
+            for (p in c.getPositions()) if (sudoku.getCell(p)!!.isNotSolved) v.add(p)
+            if (v.size == 1) {
+                /* We found an instance where only one field is empty */
+                //
+                val solutionField = v[0] //position that needs to be filled
+                //make List with all values entered in this constraint
+                val otherSolutions: MutableList<Int> = ArrayList()
+                for (p in c.getPositions()) if (p !== solutionField) otherSolutions.add(sudoku.getCell(p)!!.currentValue)
+                //make list with all possible values
+                val possibleSolutions: MutableList<Int> = ArrayList()
+                for (i in sudoku.sudokuType!!.symbolIterator) possibleSolutions.add(i)
+                /* cut away all other solutions */possibleSolutions.removeAll(otherSolutions)
+                if (possibleSolutions.size == 1) {
+                    /* only one solution remains -> there were no doubles */
+                    val solutionValue = possibleSolutions[0]
+                    return SolveActionLastDigit(solutionField, solutionValue, c.getPositions())
+                }
+            }
+        }
+        return null
+    }
+}
---
+++ sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/solvingAssistant/SolveAction.kt
@@ -0,0 +1,8 @@
+package de.sudoq.model.solvingAssistant
+
+/**
+ * Created by timo on 27.04.16.
+ */
+open class SolveAction {
+    protected var message: String? = null
+}
