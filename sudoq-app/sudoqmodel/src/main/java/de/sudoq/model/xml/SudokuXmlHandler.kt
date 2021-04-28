755f577388e35a9c47bfce4de98a80b05bb29204
timo <timo-a@gmx.ch>
2021-04-28 23:19:51 +0200

    convert GameXmlHandler, ProfileXmlHandler, SudokuXmlHandle

---
+++ sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/xml/SudokuXmlHandler.kt
@@ -0,0 +1,48 @@
+/*
+ * SudoQ is a Sudoku-App for Adroid Devices with Version 2.2 at least.
+ * Copyright (C) 2012  Heiko Klare, Julian Geppert, Jan-Bernhard Kordaß, Jonathan Kieling, Tim Zeitz, Timo Abele
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.sudoq.model.xml
+
+import de.sudoq.model.files.FileManager
+import de.sudoq.model.sudoku.Sudoku
+import de.sudoq.model.sudoku.complexity.Complexity
+import de.sudoq.model.sudoku.sudokuTypes.SudokuTypes
+import java.io.File
+
+/**
+ * Der XmlHandler zum Laden und Speichern von Sudokus
+ */
+open class SudokuXmlHandler
+/**
+ * Erzeugt einen SudokuXmlHandler, der ein neues sudoku file anlegt
+ */ @JvmOverloads constructor(private val type: SudokuTypes? = null, private val complexity: Complexity? = null) : XmlHandler<Sudoku?>() {
+    /**
+     * {@inheritDoc}
+     */
+    protected override fun getFileFor(s: Sudoku): File {
+        return if (type != null && complexity != null) {
+            FileManager.getRandomSudoku(type, complexity)
+        } else if (s.id <= 0) {
+            FileManager.getNewSudokuFile(s)
+        } else {
+            FileManager.getSudokuFile(s)
+        }
+    }
+
+    override fun modifySaveTree(tree: XmlTree) {
+        tree.addAttribute(XmlAttribute("id", file!!.name.substring(7, file!!.name.length - 4)))
+    }
+    /**
+     * Erzeugt einen SudokuXmlHandler zum Laden eines zufälligen Sudokus mit den
+     * gegebenen Parametern
+     *
+     * @param type
+     * der Typ des zu ladenden Sudokus
+     * @param complexity
+     * die Schwierigkeit des zu ladenden Sudokus
+     */
+}
