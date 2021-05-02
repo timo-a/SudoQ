86d7c5aa601f38cfd1357c4cc2050d30bcabc577
timo <timo-a@gmx.ch>
2021-05-02 14:16:05 +0200

    group init, clean into own class

---
+++ sudoq-app/sudoqmodel/src/test/java/de/sudoq/model/TestWithInitCleanforSingletons.java
@@ -0,0 +1,29 @@
+package de.sudoq.model;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+
+import de.sudoq.model.files.FileManager;
+
+public class TestWithInitCleanforSingletons {
+
+	@BeforeClass
+	public static void init() {
+		Utility.copySudokus();
+		System.out.println("hu - HA!");
+	}
+
+	@AfterClass
+	public static void clean() throws IOException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		java.lang.reflect.Field f = FileManager.class.getDeclaredField("profiles");
+		f.setAccessible(true);
+		f.set(null, null);
+		java.lang.reflect.Field s = FileManager.class.getDeclaredField("sudokus");
+		s.setAccessible(true);
+		s.set(null, null);
+		FileManager.deleteDir(Utility.profiles);
+		FileManager.deleteDir(Utility.sudokus);
+	}
+}
