9dae844020a11542202059bfcc00eb484893d816
timo <timo-a@gmx.ch>
2021-06-12 14:27:17 +0200

    refactor sudokuType into repo

--- sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/sudoku/sudokuTypes/SudokuType.kt
+++ sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/sudoku/sudokuTypes/SudokuType.kt
@@ -23,10 +23,11 @@ import java.util.*
  * A SudokuType represents the Attributes of a specific sudoku type.
  * This includes especially the Constraints that describe a sudoku type.
  */
-open class SudokuType : Iterable<Constraint>, ComplexityFactory, Xmlable {
+open class SudokuType : Iterable<Constraint>, ComplexityFactory {
 
     /** Enum holding this Type */
-    open var enumType: SudokuTypes? = null //TODO make fillFromXML statis - make this nonnullable
+    open var enumType: SudokuTypes? = null
+    //TODO make fillFromXML statis - make this nonnullable
 
     /** The ratio of fields that are to be allocated i.e. already filled when starting  a sudoku game  */
     @JvmField
@@ -71,24 +72,18 @@ open class SudokuType : Iterable<Constraint>, ComplexityFactory, Xmlable {
      * All [Position]s that are contained in at least one constraint.
      * (For a Samurai sudoku not all positions are part of a constraint)
      */
-    protected var positions: MutableList<Position>
+    protected lateinit var positions: MutableList<Position>
 
     /**
      * list of admissible permutations for this sudoku type
      */
     var permutationProperties: List<PermutationProperties>
 
-    protected var helperList: MutableList<Helpers>
+    var helperList: MutableList<Helpers>
     @JvmField
     var ccb: ComplexityConstraintBuilder
 
-    constructor() {
-        constraints = ArrayList()
-        positions = ArrayList()
-        permutationProperties = SetOfPermutationProperties()
-        helperList = ArrayList()
-        ccb = ComplexityConstraintBuilder()
-    }
+
 
     /**
      * Creates a SudokuType
@@ -111,6 +106,29 @@ open class SudokuType : Iterable<Constraint>, ComplexityFactory, Xmlable {
         permutationProperties = SetOfPermutationProperties()
         helperList = ArrayList()
         ccb = ComplexityConstraintBuilder()
+    }
+
+    /**
+     * used to initialize from SudokuTypeBE
+     */
+    constructor(enumType : SudokuTypes,
+                numberOfSymbols : Int,
+                size : Position,
+                blockSize: Position,
+                constraints: MutableList<Constraint>,
+                permutationProperties: List<PermutationProperties>,
+                helperList: MutableList<Helpers>,
+                ccb: ComplexityConstraintBuilder) {
+        this.enumType = enumType
+        this.numberOfSymbols = numberOfSymbols
+        this.standardAllocationFactor = standardAllocationFactor
+        this.size = size
+        this.blockSize = blockSize
+        this.constraints = constraints
+        this.permutationProperties = permutationProperties
+        this.helperList = helperList
+        this.ccb = ccb
+        initPositionsList()
     }
 
     /**
@@ -218,70 +236,10 @@ open class SudokuType : Iterable<Constraint>, ComplexityFactory, Xmlable {
         constraints.add(c)
     }
 
-    override fun toXmlTree(): XmlTree {
-        val representation = XmlTree("sudokutype")
-        representation.addAttribute(XmlAttribute("typename", "" + enumType!!.ordinal))
-        representation.addAttribute(XmlAttribute("numberOfSymbols", "" + numberOfSymbols))
-        representation.addAttribute(XmlAttribute("standardAllocationFactor", standardAllocationFactor.toString()))
-        representation.addChild(size!!.toXmlTree("size"))
-        representation.addChild(blockSize.toXmlTree("blockSize"))
-        for (c in constraints) {
-            representation.addChild(c.toXmlTree())
-        }
-        representation.addChild((permutationProperties as SetOfPermutationProperties).toXmlTree())
-        val hList = XmlTree("helperList")
-        for (i in helperList.indices) {
-            hList.addAttribute(XmlAttribute("i", "" + helperList[i].ordinal))
-        }
-        representation.addChild(hList)
-        representation.addChild(ccb.toXmlTree())
-
-        // TODO complexity builderdata
-        return representation
-    }
-
-    @Throws(IllegalArgumentException::class)
-    override fun fillFromXml(xmlTreeRepresentation: XmlTree) {
-        enumType = SudokuTypes.values()[xmlTreeRepresentation.getAttributeValue("typename")!!.toInt()]
-        numberOfSymbols = xmlTreeRepresentation.getAttributeValue("numberOfSymbols")!!.toInt()
-        standardAllocationFactor = xmlTreeRepresentation.getAttributeValue("standardAllocationFactor")!!.toFloat()
-        for (sub in xmlTreeRepresentation) {
-            when (sub.name) {
-                "size" -> size = Position.fillFromXmlStatic(sub)
-                "blockSize" -> blockSize = Position.fillFromXmlStatic(sub)
-                "constraint" -> {
-                    val c = Constraint(UniqueConstraintBehavior(), ConstraintType.LINE)
-                    c.fillFromXml(sub)
-                    constraints.add(c)
-                }
-                SetOfPermutationProperties.SET_OF_PERMUTATION_PROPERTIES -> {
-                    permutationProperties = SetOfPermutationProperties()
-                    (permutationProperties as SetOfPermutationProperties).fillFromXml(sub) //cast neccessary because setOPP is defined as 
-                }
-                "helperList" -> {
-                    helperList = ArrayList(sub.numberOfAttributes)
-                    val jterator = sub.getAttributes()
-                    while (jterator.hasNext()) {
-                        val xa = jterator.next()
-                        val index = xa.name.toInt()
-                        val h = Helpers.values()[xa.value.toInt()]
-                        helperList[index] = h
-                    }
-                }
-                ComplexityConstraintBuilder.TITLE -> {
-                    ccb = ComplexityConstraintBuilder()
-                    ccb.fillFromXml(sub)
-                }
-                else -> {
-                }
-            }
-        }
-        initPositionsList()
-    }
-
     private fun initPositionsList() {
         positions = ArrayList()
         for (c in constraints) for (p in c) if (!positions.contains(p)) positions.add(p)
     }
+
 
 }
--- sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/sudoku/sudokuTypes/SudokuTypeProvider.kt
+++ sudoq-app/sudoqmodel/src/main/java/de/sudoq/model/sudoku/sudokuTypes/SudokuTypeProvider.kt
@@ -1,45 +1,23 @@
 package de.sudoq.model.sudoku.sudokuTypes
 
+import de.sudoq.model.persistence.xml.sudokuType.SudokuTypeMapper
+import de.sudoq.model.persistence.xml.sudokuType.SudokuTypeRepo
 import de.sudoq.model.xml.XmlHelper
 import java.io.File
 
 object SudokuTypeProvider {
 
-
-
     /**
-     * Gibt die Sudoku-Typdatei für den spezifizierten Typ zurück.
-     * @param type die Typ-Id
-     * @return die entsprechende Sudoku-Typdatei
-     */
-    private fun getSudokuTypeFile(type: SudokuTypes, sudokuDir: File): File {
-        //getDir(getString(R.string.path_rel_sudokus), MODE_PRIVATE)
-        val ap = sudokuDir.absolutePath
-        return File(ap + File.separator + type.toString() + File.separator + type.toString() + ".xml")
-    }
-
-    /**
-     * Creates and returns a SudokuType subject to the specified Type Name.
-     * If the type cannot be mapped to a type null is returned.
+     * Returns a SudokuType subject to the specified Type Name.
+     * If the type cannot be mapped to a type an exception will be thrown.
      *
      * @param type Enum Type of the SudokuType to create.
      * @return a [SudokuType] of null if type cannot be mapped
      */
-    @JvmStatic //todo get a cleaner solution with repo?
-    fun getSudokuType(type: SudokuTypes, sudokuDir: File): SudokuType? {
-        val f = getSudokuTypeFile(type, sudokuDir)
-        if (!f.exists()) {
-            return null
-        }
-        val helper = XmlHelper()
-        try {
-            val t = SudokuType()
-            val xt = helper.loadXml(f)!!
-            t.fillFromXml(xt)
-            return t
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
+    @JvmStatic
+    fun getSudokuType(type: SudokuTypes, sudokuDir: File): SudokuType {
+        val repo = SudokuTypeRepo(sudokuDir)
+        val stBE = repo.read(type.ordinal)
+        return SudokuTypeMapper.fromBE(stBE)
     }
 }
