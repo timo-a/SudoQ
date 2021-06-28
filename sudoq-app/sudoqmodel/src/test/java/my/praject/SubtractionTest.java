b2e4e9ec37e16124f7e5b9a41aa09e74357505c1
timo <timo-a@gmx.ch>
2021-06-29 00:14:39 +0200

    remove unnecessary stuff

---
+++ sudoq-app/sudoqmodel/src/test/java/my/praject/SubtractionTest.java
@@ -0,0 +1,13 @@
+package my.praject;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SubtractionTest {
+
+    @Test
+    public void testSubtraction(){
+        Assertions.assertEquals(3, 8-5);
+    }
+}
