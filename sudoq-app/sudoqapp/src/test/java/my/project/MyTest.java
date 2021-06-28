5227ced4e68e185c6403a691f56620e83617fab1
timo <timo-a@gmx.ch>
2021-06-28 23:11:07 +0200

    move myTest to right package

---
+++ sudoq-app/sudoqapp/src/test/java/my/project/MyTest.java
@@ -0,0 +1,14 @@
+package my.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void testAddition() {
+        Assertions.assertTrue(2 + 2 == 4);
+    }
+
+}
+
