516bbd66a07816c8e9f3fba0faf73de46082bc8d
timo <1398557+timo-a@users.noreply.github.com>
2025-12-31 11:47:51 +0100

    fix backtracking tests

--- sudoq-app/sudoqmodel/src/test/java/de/sudoq/model/solverGenerator/solver/BacktrackingTests.java
+++ sudoq-app/sudoqmodel/src/test/java/de/sudoq/model/solverGenerator/solver/BacktrackingTests.java
@@ -7,15 +7,24 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import de.sudoq.model.TestWithInitCleanforSingletons;
 import de.sudoq.model.solverGenerator.solution.SolveDerivation;
 import de.sudoq.model.solverGenerator.solver.helper.Backtracking;
+import de.sudoq.model.solverGenerator.utils.PrettySudokuRepo2;
+import de.sudoq.model.solverGenerator.utils.SudokuTypeRepo4Tests;
 import de.sudoq.model.sudoku.Position;
 import de.sudoq.model.sudoku.Sudoku;
 import de.sudoq.model.sudoku.complexity.Complexity;
 import de.sudoq.model.sudoku.sudokuTypes.TypeBuilder;
 
 public class BacktrackingTests extends TestWithInitCleanforSingletons {
+
+    private static SudokuTypeRepo4Tests sudokuTypeRepo = new SudokuTypeRepo4Tests();
+
+    private PrettySudokuRepo2 sudokuRepo = new PrettySudokuRepo2(sudokuTypeRepo);
 
 	@Test
 	public void testInitialisation() {
@@ -41,16 +50,18 @@ public class BacktrackingTests extends TestWithInitCleanforSingletons {
 		assertEquals(deriv.getCellIterator().next().getPosition(), Position.get(1, 3));
 	}
 
-	@Test(timeout = 60)
-	public void testAlreadySolved() {
-		SolverSudoku sudoku = new SolverSudoku(new Sudoku(TypeBuilder.get99()));
-		sudoku.setComplexity(Complexity.arbitrary);
-		assertTrue(new Solver(sudoku).solveAll(false, true));
+	@Test(timeout = 6000)
+	public void testAlreadySolved() {//todo can we do this with 4x4 (too) so it is simpler?
+        Path sudokuPath = Paths.get("sudokus/x_easy_1.pretty");
+        Sudoku sudoku = sudokuRepo.read(sudokuPath, Complexity.easy);
+        Solver solver = new Solver(sudoku);
+        SolverSudoku solverSudoku = solver.getSolverSudoku();//todo once we have defined the output of the solver we can omit solving a soduku first
+        assertTrue(solver.solveAll(false, false, false));
 		//TODO see if sudoku can be mocked better, use smaller sudoku, understand why no return false
-		Backtracking back = new Backtracking(sudoku, 10);
-		sudoku.updateCandidates();
+		Backtracking back = new Backtracking(solverSudoku, 10);
+		solverSudoku.updateCandidates();
 		// there should be no fields to solve any more
-		assertFalse(back.update(false));
+        boolean result = back.update(false);
+        assertFalse(result);
 	}
-
 }
