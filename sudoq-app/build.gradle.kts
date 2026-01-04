a4ab0c7ba0f691c98c1d27fb623b35d81ab70272
timo <1398557+timo-a@users.noreply.github.com>
2026-01-04 23:54:40 +0100

    migrate to the compilerOptions DSL

--- sudoq-app/build.gradle.kts
+++ sudoq-app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
     }
 
 
