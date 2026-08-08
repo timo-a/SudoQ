e3aff076327c5cdfe2a60d81a1e0723a442fbe24
timo <1398557+timo-a@users.noreply.github.com>
2026-08-08 12:59:35 +0200

    hotfix class cast exception

--- sudoq-app/sudoqmodel/src/main/kotlin/de/sudoq/model/solverGenerator/solver/helper/NoNotesHelper.kt
+++ sudoq-app/sudoqmodel/src/main/kotlin/de/sudoq/model/solverGenerator/solver/helper/NoNotesHelper.kt
@@ -14,25 +14,24 @@ class NoNotesHelper(sudoku: SolverSudoku, complexity: Int) : SolveHelper(sudoku,
     override fun update(buildDerivation: Boolean): Boolean {
         var foundOne = false
         var candidate: Position
-        val emptyPos = Vector<Position>()
-        for (p in sudoku.sudokuType.validPositions) if (sudoku.getCell(p).isCompletelyEmpty) emptyPos.add(
-            p
-        )
-        foundOne = emptyPos.size > 0
+        val emptyPos = sudoku.sudokuType.validPositions
+            .filter { sudoku.getCell(it).isCompletelyEmpty }
+            .toCollection(Vector<Position>())
+        foundOne = emptyPos.isNotEmpty()
         if (buildDerivation) {
             //Todo replace by filling in all notes and apply other helpers repeatedly? but how can we limit this to allempty positions?
             //create map from pos to constraint
             val emptyPosSet: Set<Position> = HashSet(emptyPos)
             val cmap: MutableMap<Position, MutableList<Constraint>> = HashMap()
-            for (c in sudoku.sudokuType) if (c.hasUniqueBehavior()) for (p in c.getPositions()) if (emptyPosSet.contains(
-                    p
-                )
-            ) if (cmap.containsKey(p)) cmap[p]!!.add(c) else cmap[p] = ArrayList(listOf(c))
+            for (c in sudoku.sudokuType)
+                if (c.hasUniqueBehavior())
+                    for (p in c.getPositions())
+                        if (emptyPosSet.contains(p))
+                            if (cmap.containsKey(p))
+                                cmap[p]!!.add(c)
+                            else cmap[p] = ArrayList(listOf(c))
             derivation = NoNotesDerivation()
-            val allSymbols: Set<Int> =
-                HashSet(sudoku.sudokuType.symbolIterator as MutableCollection<out Int>)
-            //for(int i : sudoku.getSudokuType().getSymbolIterator())
-            //    allSymbbols.add(i);
+            val allSymbols: Set<Int> = sudoku.sudokuType.symbolIterator.toSet()
             for (p in emptyPos) {
                 val allCandidates: MutableSet<Int> = HashSet(allSymbols)
                 for (c in cmap[p]!!) {
